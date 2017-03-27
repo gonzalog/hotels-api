815a5be40745dad5b7a7fbf27af89e76408cefb9
Chalo <gonzalogarcia93@gmail.com>
2017-03-27 17:48:46 -0300

    Accept no port param

--- app/index.js
+++ app/index.js
@@ -3,13 +3,12 @@ const graphqlHTTP = require('express-graphql');
 const { schema } = require('./schema.js')
 
 const app = express();
-const PORT = process.env.PORT || 4000
 
 app.use('/', graphqlHTTP({
   schema: schema,
   graphiql: true
 }));
 
+const PORT = process.env.PORT || 4000
 app.listen(PORT)
 console.log(`Listening on port ${PORT}`)
-console.log(process.env)
