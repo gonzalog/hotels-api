fcf68a6a87409ec98c1f35057adb6a62f612e4fb
Chalo <gonzalogarcia93@gmail.com>
2017-03-29 17:32:07 -0300

    Accept cors

--- app/index.js
+++ app/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const { schema } = require('./schema.js')
+const cors = require('cors');
 
 const app = express();
 
-app.use('/', graphqlHTTP({
+app.use('/', cors(), graphqlHTTP({
   schema: schema,
   graphiql: true
 }));
